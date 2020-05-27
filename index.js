ba3d9e465c111480fdb6d7c0f24f69e30778f660
cheerful-architect-4052 <scrian_12@yahoo.com>
2020-05-27 19:59:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ function divide(a, b){
   let result4 = a / b;
   return result4;
 }
+
+function increment(n){
+  let incremented = n++;
+  return incremented;
+}
+
+function decrement(n){
+  let decremented = n--;
+  return decremented;
+}
